886f3f2bcc710b0b2c10311359d8853a96a7a91c
Streamxxx <67567729+Streamxxx@users.noreply.github.com>
2021-11-04 12:24:13 -0500

    Add files via upload

---
+++ allColors.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+def dibujar(mask, color):
+    contornos, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    #cv2.drawContours(frame, contornos, -1 ,(255,0,0), 3)
+    for c in contornos:
+        area=cv2.contourArea(c)
+        #Aumentar para alejar mas
+        if area > 1000:
+            M = cv2.moments(c)
+            if (M["m00"]==0): 
+                M["m00"]=1
+            x = int(M["m10"]/M["m00"])
+            y = int(M['m01']/M['m00'])
+            nuevoContorno=cv2.convexHull(c)
+            cv2.circle(frame, (x,y), 4, (0,255,0), -1)
+            cv2.putText(frame, '{},{}'.format(x,y),(x+10,y), font, 0.75,(0,255,255),1,cv2.LINE_AA)
+            cv2.drawContours(frame, [nuevoContorno], 0 ,color, 3)
+
+cap = cv2.VideoCapture(0)
+
+azulBajo = np.array([100,100,20], np.uint8)
+azulAlto = np.array([125,255,255], np.uint8)
+
+verdeBajo = np.array([45,100,20], np.uint8)
+verdeAlto = np.array([75,255,255], np.uint8)
+
+rojoBajo1 = np.array([0,100,20], np.uint8)
+rojoAlto1 = np.array([8,255,255], np.uint8)
+rojoBajo2 = np.array([170,100,20], np.uint8)
+rojoAlto2 = np.array([179,255,255], np.uint8)
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while True:
+    ret, frame = cap.read()
+    if ret == True:
+        frameHSV=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        maskAzul = cv2.inRange(frameHSV,azulBajo,azulAlto)
+        maskGreen = cv2.inRange(frameHSV,verdeBajo,verdeAlto)
+        maskRed1 =cv2.inRange(frameHSV,rojoBajo1,rojoAlto1)
+        maskRed2 =cv2.inRange(frameHSV,rojoBajo2,rojoAlto2)
+        maskRed = cv2.add(maskRed1,maskRed2)
+        dibujar(maskAzul,(255,0,0))
+        dibujar(maskGreen,(0,143,57))
+        dibujar(maskRed,(0,0,255))
+        cv2.imshow('frame',frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            break
+cap.release()
+cv2.destroyAllWindows()
---
+++ prueba2.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+redBajo1 = np.array([0, 100, 20], np.uint8)
+redAlto1 = np.array([8, 255, 255], np.uint8)
+redBajo2=np.array([175, 100, 20], np.uint8)
+redAlto2=np.array([179, 255, 255], np.uint8)
+blueBajo = np.array([105, 100, 20], np.uint8)
+blueAlto = np.array([125, 255, 255], np.uint8)
+
+while True:
+  ret,frame=cap.read()
+  if ret==True:
+    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    maskRed1 = cv2.inRange(frameHSV, redBajo1, redAlto1)
+    maskRed2 = cv2.inRange(frameHSV, redBajo2, redAlto2)
+    maskRed = cv2.add(maskRed1, maskRed2)
+    maskBlue = cv2.inRange(frameHSV, blueBajo, blueAlto)
+
+    #Obtencion del color
+    maskRedvis = cv2.bitwise_and(frame, frame, mask= maskRed)        
+    cv2.imshow('frame', frame)
+
+    #CONTORNOS
+    contorno, _ = cv2.findContours(maskRed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contorno, -1, (0,0,0), 2)
+    #BLANCO Y NEGRO
+    cv2.imshow('maskRed', maskRed)
+    cv2.imshow('maskRedvis', maskRedvis)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+      break
+cap.release()
+cv2.destroyAllWindows()
